7b8ae98f2ae334e246508f852e55e1aa047d6459
Jose Rafael Arrieta Dominguez <jose.arrieta@udea.edu.co>
2021-02-09 20:08:16 -0500

    Update main.go

--- mvc/main.go
+++ mvc/main.go
@@ -1,7 +1,23 @@
-package main
+/*package main
 
 import "app"
 
 func main() {
 	app.StartApp()
+}*/
+
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", HelloServer)
+    http.ListenAndServe(":80", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, World!")
 }
